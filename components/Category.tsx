bd0615d5589cc8a33f7eb840f2be1d06db8dfcb8
fadykhayratblb <fady.mehany@btech.com>
2024-03-13 04:10:05 +0200

    Update Category.tsx

--- components/Category.tsx
+++ components/Category.tsx
@@ -14,7 +14,7 @@ export default function Category({ setSelectedTag, tag, selectedTag }) {
         selectedTag === tag && 'ring-2 ring-gray-400 text-gray-700'
       } inline-flex items-center px-3 py-1.5 uppercase bg-gray-100 rounded cursor-pointer`}
     >
-      <span className="text-xs font-medium uppercase">{tag || 'All'}</span>
+      <span className="text-xs font-medium uppercase">{tag || 'كله'}</span>
     </div>
   );
 }
