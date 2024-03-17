06d26f4fa350213fe2998f65a902624dc9bdd757
fadykhayratblb <fady.mehany@btech.com>
2024-03-17 23:43:32 +0200

    Revert "working with google tag"
    This reverts commit fefe6c97cc25921faf06f50e92871198568b3506.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,28 +1,10 @@
 import 'styles/globals.css';
 
-import * as gtag from './gtag';
-
 import type { AppProps } from 'next/app';
-import { GA_TRACKING_ID } from './gtag';
-import Head from 'next/head';
 import Script from 'next/script';
 import { Toaster } from 'react-hot-toast';
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-
-  useEffect(() => {
-    const handleRouteChange = (url: URL) => {
-      gtag.pageview(url);
-    };
-    router.events.on('routeChangeComplete', handleRouteChange);
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-    };
-  }, [router.events]);
-
   return (
     <>
       {process.env.NEXT_PUBLIC_UMAMI_ID &&
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,38 +1,10 @@
 import { Head, Html, Main, NextScript } from 'next/document';
 
-import { GA_TRACKING_ID } from './gtag';
-import Script from 'next/script';
-
 export default function Document() {
   return (
     <Html lang="ar" dir="rtl">
-      {/* <Head /> */}
-      <Head>
-        {/* enable analytics script only for production */}
-        {
-          <>
-            <Script
-              async
-              id="google"
-              src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-            />
-            <Script
-              id="google-analytics"
-              // eslint-disable-next-line react/no-danger
-              dangerouslySetInnerHTML={{
-                __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `
-              }}
-            />
-          </>
-        }
-      </Head>
+      <Head />
+
       <body>
         <Main />
         <NextScript />
