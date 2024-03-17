9c7058f7a42d9db80fa62f7eaecd46a4e37600eb
fadykhayratblb <fady.mehany@btech.com>
2024-03-17 18:27:59 +0200

    analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,3 +1,5 @@
+import * as gtag from './gtag';
+
 import { Head, Html, Main, NextScript } from 'next/document';
 
 import Script from 'next/script';
@@ -5,23 +7,30 @@ import Script from 'next/script';
 export default function Document() {
   return (
     <Html lang="ar" dir="rtl">
-      <Script
+      {/* <Script
         id="my-script"
         strategy="lazyOnload"
         src={`https://www.googletagmanager.com/gtag/js?id=G-KQ7JK87CF9`}
-      />
+      /> */}
 
-      <Script strategy="lazyOnload" id="my-script2">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-KQ7JK87CF9',{
-          page_path: window.location.pathname,
-          
-![image](https://cdn.sanity.io/images/dgsq0x0m/production/61f24596fb210134e5df6a1dfa285de0f24791cc-1549x601.png?w=450)});
-         `}
-      </Script>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `
+        }}
+      />
 
       <Head>
         <title>Welcome!</title>
