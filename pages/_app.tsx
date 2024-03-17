c39c43f319972f70fc26d928e48deeeee23ff38d
fadykhayratblb <fady.mehany@btech.com>
2024-03-17 23:43:50 +0200

    Revert "working with google tag"
    This reverts commit fefe6c97cc25921faf06f50e92871198568b3506.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,7 @@
-import 'styles/globals.css';
-
 import type { AppProps } from 'next/app';
 import Script from 'next/script';
 import { Toaster } from 'react-hot-toast';
+import 'styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
