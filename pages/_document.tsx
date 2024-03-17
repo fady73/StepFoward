a7709a73f01ed40324bec8a65618cc7a719fce40
fadykhayratblb <fady.mehany@btech.com>
2024-03-17 18:34:56 +0200

    analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,3 @@
-import * as gtag from './gtag';
-
 import { Head, Html, Main, NextScript } from 'next/document';
 
 import Script from 'next/script';
@@ -7,36 +5,25 @@ import Script from 'next/script';
 export default function Document() {
   return (
     <Html lang="ar" dir="rtl">
-      {/* <Script
-        id="my-script"
-        strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=G-KQ7JK87CF9`}
-      /> */}
+      <head>
+        <Script
+          async
+          id="google"
+          src="https://www.googletagmanager.com/gtag/js?id=G-3YYLPMGX7W"
+        />
 
-      <Script
-        id="gtag-init1"
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-      />
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${gtag.GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `
-        }}
-      />
-
-      <Head>
-        <title>Welcome!</title>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-      </Head>
+        <Script id="google-analytics">
+          {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', ${'G-3YYLPMGX7W'},{
+        page_path: window.location.pathname,
+      });
+  `}
+        </Script>
+      </head>
+      <Head />
 
       <body>
         <Main />
--- pages/gtag.ts
+++
@@ -1,1 +0,0 @@
-export const GA_TRACKING_ID = 'G-KQ7JK87CF9'; //replace it with your measurement id
