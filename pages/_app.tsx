e652be96dc037237912368a7db4b9561511e3670
fadykhayratblb <fady.mehany@btech.com>
2024-03-17 23:36:00 +0200

    updates

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,28 @@
 import 'styles/globals.css';
 
+import * as gtag from './gtag';
+
 import type { AppProps } from 'next/app';
+import { GA_TRACKING_ID } from './gtag';
+import Head from 'next/head';
 import Script from 'next/script';
 import { Toaster } from 'react-hot-toast';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
       {process.env.NEXT_PUBLIC_UMAMI_ID &&
@@ -15,21 +33,6 @@ export default function App({ Component, pageProps }: AppProps) {
             src={process.env.NEXT_PUBLIC_UMAMI_URL}
           />
         )}
-      <Script
-        async
-        id="google"
-        src="https://www.googletagmanager.com/gtag/js?id=G-3YYLPMGX7W"
-      />
-      <Script id="google-analytics">
-        {`
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config','G-3YYLPMGX7W',{
-        page_path: window.location.pathname,
-      });
-  `}
-      </Script>
       <Toaster />
       <Component {...pageProps} />;
     </>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,36 @@
 import { Head, Html, Main, NextScript } from 'next/document';
 
+import { GA_TRACKING_ID } from './gtag';
 import Script from 'next/script';
 
 export default function Document() {
   return (
     <Html lang="ar" dir="rtl">
-      <Head />
+      {/* <Head /> */}
+      <Head>
+        {/* enable analytics script only for production */}
+        {
+          <>
+            <script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+            />
+            <script
+              // eslint-disable-next-line react/no-danger
+              dangerouslySetInnerHTML={{
+                __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `
+              }}
+            />
+          </>
+        }
+      </Head>
       <body>
         <Main />
         <NextScript />
