fefe6c97cc25921faf06f50e92871198568b3506
fadykhayratblb <fady.mehany@btech.com>
2024-03-17 18:44:10 +0200

    working with google tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,26 +5,7 @@ import Script from 'next/script';
 export default function Document() {
   return (
     <Html lang="ar" dir="rtl">
-      <head>
-        <Script
-          async
-          id="google"
-          src="https://www.googletagmanager.com/gtag/js?id=G-3YYLPMGX7W"
-        />
-
-        <Script id="google-analytics">
-          {`
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', ${'G-3YYLPMGX7W'},{
-        page_path: window.location.pathname,
-      });
-  `}
-        </Script>
-      </head>
       <Head />
-
       <body>
         <Main />
         <NextScript />
