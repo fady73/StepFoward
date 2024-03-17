c6728a0c229124a45884e91640fc3299cac21aa5
fadykhayratblb <fady.mehany@btech.com>
2024-03-17 18:18:17 +0200

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,32 @@
 import { Head, Html, Main, NextScript } from 'next/document';
 
+import Script from 'next/script';
+
 export default function Document() {
   return (
     <Html lang="ar" dir="rtl">
-      <Head />
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-KQ7JK87CF9`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-KQ7JK87CF9',{
+          page_path: window.location.pathname,
+          
+![image](https://cdn.sanity.io/images/dgsq0x0m/production/61f24596fb210134e5df6a1dfa285de0f24791cc-1549x601.png?w=450)});
+         `}
+      </Script>
+
+      <Head>
+        <title>Welcome!</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+
       <body>
         <Main />
         <NextScript />
