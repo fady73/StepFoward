4627e33e81c7c0a9529458b1b7924c254c8c8b7e
Your New Name <163141137+fadykahyratGa@users.noreply.github.com>
2024-04-11 16:11:03 +0200

    Update index.tsx

--- pages/book/index.tsx
+++ pages/book/index.tsx
@@ -4,13 +4,13 @@ import PDFViewer from 'components/PDFViewer';
 export default function Index(props) {
   return (
     <Layout title={'كتاب 500 لعبه'} description={'summary'} ogUrl={`/book`}>
-      <div class=" bg-gray-100">
+      <div className=" bg-gray-100">
         <div style={{ paddingTop: '20px' }}>
-          <h2 class="text-lg pr-6">
+          <h2 className="text-lg pr-6">
             {' '}
             ده كتاب فى 500 لعبه مناسبه للحفلات والايام الروحيه الخاصه بمدارس الاحد الكتاب
             اعداد الاخت ساميه برتى ربنا يبارك تعبها ويثمر فى خدمه مدارس الاحد{' '}
-            <a href="/sundaybook.pdf" class="underline  underline-offset-1	text-sky-500	" target="_blank" rel="noopener noreferrer" download>
+            <a href="/sundaybook.pdf" className="underline  underline-offset-1	text-sky-500	" target="_blank" rel="noopener noreferrer" download>
               تقدر تحمله من هنا
             </a>
           </h2>
