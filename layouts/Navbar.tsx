f12717702584941c542354cbc159f19ae5da5b26
Your New Name <163141137+fadykahyratGa@users.noreply.github.com>
2024-04-25 05:02:31 +0200

    Update Navbar.tsx

--- layouts/Navbar.tsx
+++ layouts/Navbar.tsx
@@ -64,7 +64,7 @@ export default function Navbar() {
             </div>
           </div>
 
-          <div className="fixed bottom-0 left-4 z-50 w-full h-16 bg-white border-t border-gray-200 dark:bg-gray-700 dark:border-gray-600">
+          <div className="fixed bottom-0 left-0 z-50 w-full h-16 bg-white border-t border-gray-200 dark:bg-gray-700 dark:border-gray-600">
             <div className="grid h-full max-w-lg grid-cols-2 mx-auto font-medium">
               <Link
                 className="inline-flex flex-col items-center justify-center px-5 border-gray-200 border-x hover:bg-gray-50 dark:hover:bg-gray-800 group dark:border-gray-600"
