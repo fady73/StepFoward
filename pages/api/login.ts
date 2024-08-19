72c2accdff2e4f53828d6b050d7105cb67e93a7d
fadykhayratblb <fady.mehany@btech.com>
2024-08-19 18:41:57 +0300

    login

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -23,8 +23,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   if (!email || !password) {
     return res.status(400).json({ error: 'Missing required fields' });
   }
-
-  const user = await User.findOne({ email }).exec(); // Use .exec() to get a promise
+ // @ts-ignore
+  const user = await User.findOne({ email });
 
   if (!user) {
     return res.status(404).json({ error: 'المستخدم غير موجود' });
--- pages/api/reset-password.ts
+++ pages/api/reset-password.ts
@@ -21,7 +21,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     return res.status(400).json({ error: 'Missing required fields' });
   }
 
-  const user = await User.findOne({ email });
+
+ // @ts-ignore
+ const user = await User.findOne({ email });
   if (!user) {
     return res.status(404).json({ error: 'User not found' });
   }
