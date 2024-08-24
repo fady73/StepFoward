8958974d8c2c542f472d18014426345313c029d6
fadykhayratblb <fady.mehany@btech.com>
2024-08-24 03:41:47 +0300

    CORS

--- pages/api/waiting-list.ts
+++ pages/api/waiting-list.ts
@@ -3,11 +3,18 @@
 
 import type { NextApiRequest, NextApiResponse } from 'next';
 
+import NextCors from 'nextjs-cors';
 import User from '../../models/user';
 import { authenticate } from '../../lib/auth';
 import dbConnect from '../../lib/mongodb';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  await NextCors(req, res, {
+    // Options
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    origin: '*',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
   if (req.method !== 'GET') {
     return res.status(405).json({ error: `Method '${req.method}' Not Allowed` });
   }
